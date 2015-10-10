e1d4f75106b8beaf5c6cd0fa11e7a6debe36dd2a
Hersh Sanghvi <hersh500@gmail.com>
2015-10-10 14:51:08 -0700

    SERVER.py

--- TEST_CLIENT.py
+++ TEST_CLIENT.py
@@ -1,8 +1,9 @@
 import requests
 
-url = 'http://127.0.0.1:5000'
+remote_url = 'https://immense-taiga-6433.herokuapp.com'
+local_url = '0.0.0.0:5000'
 add_on = '/images/api/v1.0/' 
 
 files = {'file':open('/Users/Hersh/Desktop/burger.jpg', 'rb')}
-result = requests.post (url+add_on, files=files)
+result = requests.post (remote_url+add_on, files=files)
 print (result.text)
