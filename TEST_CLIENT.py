446c526ea27518b4f33fc3b01649fafe17cb56aa
Hersh Sanghvi <hersh500@gmail.com>
2015-10-10 15:45:42 -0700

    modify

--- TEST_CLIENT.py
+++ TEST_CLIENT.py
@@ -1,7 +1,8 @@
 import requests
 
-remote_url = 'https://immense-taiga-6433.herokuapp.com'
-local_url = '0.0.0.0:5000'
+remote_url = 'https://nameless-cliffs-9474.herokuapp.com'
+local_url = 'http://127.0.0.1:5000'
+
 add_on = '/images/api/v1.0/' 
 
 files = {'file':open('/Users/Hersh/Desktop/burger.jpg', 'rb')}
