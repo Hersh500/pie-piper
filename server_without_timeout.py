ee7af0651eb295d8e297d937cccfde3b4ab2da44
Hersh Sanghvi <hersh500@gmail.com>
2015-10-11 01:20:35 -0700

    one of many

--- server_without_timeout.py
+++ server_without_timeout.py
@@ -1,6 +1,6 @@
 #Hersh Sanghvi
 
-from flask import Flask, request #Imports necessary flask libs
+from flask import Flask, request, Response #Imports necessary flask libs
 from clarifai.client import ClarifaiApi #Imports the clarifai api
 from azure.storage.blob import BlobService
 import requests
@@ -20,18 +20,18 @@ def index():
 	return "Hello, World! This is the server for our calhacks team's project"
 
 #d = {}
-@app.route('/images/api/v1.0/', methods=['POST']) #Same thing as above, except this is what Flask should do when a POST request is made to this URL
+@app.route('/bbb', methods=['POST']) #Same thing as above, except this is what Flask should do when a POST request is made to this URL
 def get_tags():
 	#TODO: Error checking
-#	global d 
-#	d = {}
+#       global d 
+#       d = {}
 
 	clarifai_api = ClarifaiApi()
-	blob_service = BlobService('calhacks', 'mm7EmY+T+MGahePBDSDU5LHpZR5tRXuh4MSco4jFrzHovOPEf06e18c89pxtPIo4NDVhhjSeaQY/FQmKNxjjyA==')	
+	blob_service = BlobService('calhacks', 'mm7EmY+T+MGahePBDSDU5LHpZR5tRXuh4MSco4jFrzHovOPEf06e18c89pxtPIo4NDVhhjSeaQY/FQmKNxjjyA==')      
 
 	blob_name = request.form['blob_id']
-#	blob_name = blob_name.decode('utf-8')
-	blob_service.get_blob_to_path('imagestore', blob_name, 'out.png')	
+#       blob_name = blob_name.decode('utf-8')
+	blob_service.get_blob_to_path('imagestore', blob_name, 'out.png')       
 	print("checkpoint 1")
 	i = open ('out.png', 'r')
 	strd = ""
@@ -44,21 +44,23 @@ def get_tags():
 
 	f = open (fname, 'rb')
 	result = clarifai_api.tag_images(f)
-	print(result)
 	st = result['results'][0]['result']['tag']['classes'][0:6]
+	print(st)
 
-	for i in ['food', 'nobody', 'still life', 'meal', 'dish', 'plate', 'delicious', 'isolated', 'cutout', 'unhealthy', 'one', 'background']: 
+	for i in []:#['food', 'nobody', 'still life', 'meal', 'dish', 'plate', 'delicious', 'isolated', 'cutout', 'unhealthy', 'one', 'background']: 
 		while i in st:
 			st.remove(i)
-	return json.dumps(search_terms(st))
-
-#	return "success!"
+	js = json.dumps(search_terms(st))
+	print(js)
+	return js
+	#return Response(js, mimetype='application/json')
+#       return "success!"
 '''
 @app.route('/images/api/v1.0/status', methods=['GET'])
 def get_status():
 	blob_name = request.data.decode('utf-8')
 	if blob_name in d.keys():
-		return json.dumps({"0":"OK"})		
+		return json.dumps({"0":"OK"})           
 	else:
 		return json.dumps({"0":"NO"})
 
@@ -88,4 +90,3 @@ def get_recipes(lst):
 
 if __name__ == "__main__":
 	app.run(debug=True)
-
