cfddc194375b409c3f8fa287f2d46f84b5b18d85
Hersh Sanghvi <hersh500@gmail.com>
2015-10-10 20:47:57 -0700

    Added Azure support

--- SERVER_AZURE.py
+++ SERVER_AZURE.py
@@ -7,6 +7,7 @@ from azure.storage.blob import BlobService
 import requests
 import json
 import base64
+import os
 
 app = Flask(__name__) #don't worry about this part
 
@@ -17,13 +18,15 @@ def index():
 @app.route('/images/api/v1.0/', methods=['POST']) #Same thing as above, except this is what Flask should do when a POST request is made to this URL
 def get_tags():
 	#TODO: Error checking
+	os.environ['CLARIFAI_APP_ID'] = '3QUAmRKkKsqo6RExP-oARAeezvTlOWqseo3LyCUe' #TODO: Make secret
+	os.environ['CLARIFAI_APP_SECRET'] = 'S4eGqB-Ym1Qu3vmfZRpT9WKHfVIcHb8CQ7hrR_lj' #TODO: Make secret
 	blob_service = BlobService('calhacks','mm7EmY+T+MGahePBDSDU5LHpZR5tRXuh4MSco4jFrzHovOPEf06e18c89pxtPIo4NDVhhjSeaQY/FQmKNxjjyA==') #TODO: hide this 
 	clarifai_api = ClarifaiApi()
 
 	blob_name = request.data
-	print (blob_name)
-	print(blob_service.list_blobs('imagestore', marker=None))
-	blob_service.get_blob_to_path('imagestore', str(blob_name), 'out.png')	
+	blob_name = blob_name.decode('utf-8')
+	#print(blob_service.list_blobs('imagestore', marker=None))
+	blob_service.get_blob_to_path('imagestore', blob_name, 'out.png')	
 	print("checkpoint 1")
 	i = open ('out.png', 'r')
 	strd = ""
@@ -51,7 +54,7 @@ def search_terms(term_list):
 	r = requests.get('http://food2fork.com/api/search', params= search_terms)
 	i = 0
 	recipe = []
-	while i < 3 or i < len(r.json()['recipes']):
+	while i < 3 and i < len(r.json()['recipes']):
 		current_rid = r.json()['recipes'][i]['recipe_id']
 		recipe.append(current_rid)
 		i += 1
